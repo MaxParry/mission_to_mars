fe25ef42a788efb325056e5e09c2adecc8826688
Max Parry <maxparry@Maxs-MacBook-Pro.local>
2018-02-26 20:33:26 -0800

    got flask app running

--- Web/app.py
+++ Web/app.py
@@ -1,46 +1,58 @@
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 from splinter import Browser
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, redirect
 import pymongo
+from flask_pymongo import PyMongo
 from scrape_mars import scrape
 
 app = Flask(__name__)
 
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
-db = client.space_stuff
-db.dropDatabase()
-db = client.space_stuff
-collection = db.mars
+mongo = PyMongo(app)
 
-db.collection.insert_one(thedict)
-
-backdict = db.items.find()
+# ok, things are working.
+# need to html a button to route to /scrape
+# need to (maybe) figure out how to run mongod automatically at terminal so site works
+# need to write bootstrap site
 
 @app.route("/")
 def index():
-
-    thedict = scrape()
+    try:
+        returned_dict = mongo.db.mars.find_one()
+    except:
+        print('No DB yet')
+        return('No DB yet, start the mongo server from shell')
 
     return render_template("index.html", 
-        output=thedict,
-        article_title=thedict['article_title'],
-        article_summary=thedict['article_summary'],
-        feature_image_url=thedict['feature_image_url'],
-        tweet=thedict['tweet'],
-        table_html=thedict['table_html'],
-        t1=thedict['hemisphere_list'][0]['title'],
-        u1=thedict['hemisphere_list'][0]['img_url'],
-        t2=thedict['hemisphere_list'][1]['title'],
-        u2=thedict['hemisphere_list'][1]['img_url'],
-        t3=thedict['hemisphere_list'][2]['title'],
-        u3=thedict['hemisphere_list'][2]['img_url'],
-        t4=thedict['hemisphere_list'][3]['title'],
-        u4=thedict['hemisphere_list'][3]['img_url']
+        output=returned_dict,
+        article_title=returned_dict['article_title'],
+        article_summary=returned_dict['article_summary'],
+        feature_image_url=returned_dict['feature_image_url'],
+        tweet=returned_dict['tweet'],
+        table_html=returned_dict['table_html'],
+        t1=returned_dict['hemisphere_list'][0]['title'],
+        u1=returned_dict['hemisphere_list'][0]['img_url'],
+        t2=returned_dict['hemisphere_list'][1]['title'],
+        u2=returned_dict['hemisphere_list'][1]['img_url'],
+        t3=returned_dict['hemisphere_list'][2]['title'],
+        u3=returned_dict['hemisphere_list'][2]['img_url'],
+        t4=returned_dict['hemisphere_list'][3]['title'],
+        u4=returned_dict['hemisphere_list'][3]['img_url']
         )
 
-if __name__=="__main__":
+@app.route("/lets_scrape")
+def lets_scrape():
+    thedict = scrape()
+    mars = mongo.db.mars
+    mars.update(
+        {},
+        thedict,
+        upsert=True
+    )
+
+    return redirect("http://localhost:5000/", code=302)
+
+if __name__ == "__main__":
     app.run(debug=True)
 
 
